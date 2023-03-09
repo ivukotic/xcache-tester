284b06220dd8e32f70b6c66e6226f0b976fd967c
Ilija Vukotic <ivukotic@cern.ch>
2023-03-09 16:33:17 -0600

    more dev

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM sslhep/rucio-client:main
 
+RUN yum install -y jq
+
 ENV X509_CERT_DIR /etc/grid-security/certificates
 
 COPY *.sh ./
--- read_data.sh
+++ read_data.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# get all XCaches
+# get all XCaches from VP
+curl -XGET https://vps.cern.ch/liveness | jq . | grep address | awk -F'"' {'print $4'} > ips.txt
 
-# which file to read?
+echo "using $1 and $2"
+while read serv; do
+    echo "$serv"
+    # xrdcp with timeout
+    # timeout 30 xrdcp -f root://$serv//$origin/xc/fn /dev/null
+done <ips.txt
 
-# xrdcp with timeout
-# timeout 30 xrdcp -f xc/fn /dev/null
