5c0d6eb18cd64fd49c22f473de0154bbd76138f8
Ilija Vukotic <ivukotic@cern.ch>
2023-03-13 15:08:22 -0500

    fix

--- create_dataset.sh
+++ create_dataset.sh
@@ -14,9 +14,12 @@ rucio add-dataset --lifetime 86400 $ds
 for i in {1..2}
 do
     fn=xcache_$1_$i.dat
+    echo 'creating file: $fn'
     dd if=/dev/zero of=$fn  bs=1M  count=1
+    echo 'uploading file: $fn'
     rucio upload --scope user.ivukotic --rse MWT2_UC_SCRATCHDISK $fn
-    rucio attach $ds $fn
+    echo 'attaching file: user.ivukotic:$fn to dataset $ds'
+    rucio attach $ds user.ivukotic:$fn
 done
 rucio list-file-replicas $ds
 
