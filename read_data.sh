dacedd271ee9942d21f710181950e45242eb075a
Ilija Vukotic <ivukotic@cern.ch>
2023-03-13 15:30:56 -0500

    f

--- read_data.sh
+++ read_data.sh
@@ -7,12 +7,14 @@ echo "using $dat and $2"
 
 ds=user.ivukotic:user.ivukotic.xcache_$dat
 # get from rucio path to the file
-fp=$(rucio list-file-replicas $ds --protocol root | grep xcache_$dat_$2.dat | awk '{print $12}')
+fp=$(rucio list-file-replicas $ds --protocol root | grep xcache_${dat}_${2}.dat | awk '{print $12}')
 echo "to read $fp"
 
 while read serv; do
     echo "testing $serv"
     # xrdcp with timeout
-    timeout 30 xrdcp -f root://$serv//$fp /dev/null
+    cmd="xrdcp -f root://$serv//$fp /dev/null"
+    echo $cmd
+    timeout 30 $cmd
 done <ips.txt
 
