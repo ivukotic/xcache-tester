fdc3a9b42ac4247e22cacf7f93a531ae69bcae2b
Ilija Vukotic <ivukotic@uchicago.edu>
2023-07-03 10:38:53 -0500

    longer proxy

--- runme.sh
+++ runme.sh
@@ -3,7 +3,7 @@
 voms-proxy-init --pwstdin -key /etc/grid-certs/userkey.pem \
                   -cert /etc/grid-certs/usercert.pem \
                   --voms=atlas \
-                  -valid "24:00" \
+                  -valid "25:00" \
                   <  /etc/grid-certs-ro/passphrase
 
 voms-proxy-info
