caf91778e327f41702c448e09bb1e68a593d18df
Ilija Vukotic <ivukotic@uchicago.edu>
2025-03-11 10:18:13 -0500

    comment out vomses option in voms-proxy-init command in runme.sh

--- runme.sh
+++ runme.sh
@@ -3,10 +3,11 @@
 voms-proxy-init3 --pwstdin -key /etc/grid-certs/userkey.pem \
                   -cert /etc/grid-certs/usercert.pem \
                   -voms atlas \
-                #   -vomses vomses \
                   -valid "25:00" \
                   <  /etc/grid-certs-ro/passphrase
 
+                #   -vomses vomses \
+                
 voms-proxy-info3
 
 export X509_USER_PROXY=/tmp/x509up_u0
